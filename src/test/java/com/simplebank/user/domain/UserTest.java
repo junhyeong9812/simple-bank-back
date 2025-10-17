908ddf74a6d0c0021c2457b40eee366799d4f328
jun <pickjog@gmail.com>
2025-10-17 16:55:13 +0900

    feat: update green UserTest

---
+++ src/test/java/com/simplebank/user/domain/UserTest.java
@@ -0,0 +1,31 @@
+package com.simplebank.user.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("User 도메인 테스트")
+class UserTest {
+    
+    @Test
+    @DisplayName("User 생성 성공")
+    void create_user() {
+        //when
+        User user = User.builder()
+                .id(1L)
+                .username("user1")
+                .password("password123")
+                .status(UserStatus.ACTIVE)
+                .build();
+
+        //Then
+        assertThat(user.getId()).isEqualTo(1L);
+        assertThat(user.getUsername()).isEqualTo("user1");
+        assertThat(user.getPassword()).isEqualTo("password123");
+        assertThat(user.getStatus()).isEqualTo(UserStatus.ACTIVE);
+    }
+
+}
