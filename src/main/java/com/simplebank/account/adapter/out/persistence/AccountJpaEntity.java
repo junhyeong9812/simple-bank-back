97720a0d7c146293c86a54e2013aedf8418f656d
jun <pickjog@gmail.com>
2025-10-25 20:03:25 +0900

    feat (AccountJpaEntity): implements AccountJpaEntity

--- src/main/java/com/simplebank/account/adapter/out/persistence/AccountJpaEntity.java
+++ src/main/java/com/simplebank/account/adapter/out/persistence/AccountJpaEntity.java
@@ -1,4 +1,47 @@
 package com.simplebank.account.adapter.out.persistence;
 
+import com.simplebank.account.domain.Account;
+import com.simplebank.account.domain.AccountStatus;
+import com.simplebank.account.domain.Money;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.EnumType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.AccessLevel;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@NoArgsConstructor( access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class AccountJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(unique = true, nullable = false)
+    private String accountNumber;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AccountStatus status;
+
 }
