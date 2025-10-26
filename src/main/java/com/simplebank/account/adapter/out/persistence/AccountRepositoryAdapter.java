932393d7688b1179cadfc3207a3d0f0c1beb0751
jun <pickjog@gmail.com>
2025-10-27 05:46:57 +0900

    feat (AccountJpaRepository): implements AccountJpaRepositoryAdapter

--- src/main/java/com/simplebank/account/adapter/out/persistence/AccountRepositoryAdapter.java
+++ src/main/java/com/simplebank/account/adapter/out/persistence/AccountRepositoryAdapter.java
@@ -1,4 +1,29 @@
 package com.simplebank.account.adapter.out.persistence;
 
-public class AccountRepositoryAdapter {
+import com.simplebank.account.application.port.out.LoadAccountPort;
+import com.simplebank.account.domain.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Repository
+@RequiredArgsConstructor
+public class AccountRepositoryAdapter implements LoadAccountPort{
+    private final AccountJpaRepository jpaRepository;
+
+    @Override
+    public Optional<Account> loadById(Long accountId) {
+        return jpaRepository.findById(accountId)
+                .map(AccountJpaEntity::toDomain);
+    }
+
+    @Override
+    public List<Account> loadByUserId(Long userId) {
+        return jpaRepository.findByUserId(userId).stream()
+                .map(AccountJpaEntity::toDomain)
+                .collect(Collectors.toList());
+    }
 }
